3f30a86df92debcf8abf2febd3d06113658397aa
jasonbourne1973 <j80696221@gmail.com>
2026-09-23 10:07:29 +0330

    Scope observation workflows to active academic year

--- views/pages/followups_page.py
+++ views/pages/followups_page.py
@@ -27,6 +27,9 @@ from PySide6.QtWidgets import (
 from dal.staff_dal import StaffDAL
 from dal.student_dal import StudentDAL
 from dal.teacher_assignment_dal import TeacherAssignmentDAL
+from dal.student_academic_profile_dal import StudentAcademicProfileDAL
+from dal.intervention_dal import InterventionDAL
+from dal.academic_year_dal import AcademicYearDAL
 from services.followup_service import FollowUpService
 from utils.logger import get_logger
 from views.dialogs.followup_form import FollowUpForm
